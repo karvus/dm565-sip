584e58fd87a971af8bc2468eb02e7b2adb4ed2d9
Thomas Stenhaug <thomas.stenhaug@gmail.com>
2019-12-17 18:46:21 +0100

    Add json files

---
+++ src/insert-recipe.py
@@ -0,0 +1,25 @@
+import json
+import mysql.connector
+import sys
+
+cnx = mysql.connector.connect(user='dm565sip', database='dm565', host='karvus.com', password='zipthat')
+cursor = cnx.cursor()
+
+recipe_sql_template = 'INSERT INTO recipe (id, title, instructions, total_energy, energy_per_serving, protein, fat, carbohydrates) VALUES ({id}, "{title}", "{instructions}", {nutrients[Totalkcal]}, {nutrients[kcal]}, {nutrients[protein]}, {nutrients[carbohydrates]};'
+ingredient_sql_template = 'INSERT INTO ingredient (recipe_id, amount, unit, name) VALUES ({id}, {amount}, "{unit}", "{name});'
+
+def compute_statements(recipe_json):
+    recipe_stmt = recipe_sql_template.format(**recipe_json)
+    ingredient_stmts = []
+    for ingredient_row in recipe_json['ingredient']:
+        ingredient_row['id'] = recipe_json['id']
+        ingredient_stmts.append(ingredient_sql_template.format(**ingredient_row))
+    return [recipe_stmt] + ingredient_stmts
+
+if __name__ == '__main__':
+    with open(sys.argv[1]) as json_file:
+        recipe_json = json.load(json_file)
+    statements = compute_statements(recipe_json)
+    for statement in statements:
+        cursor.execute(statement)
+        

ad6a37a4fefc78eee7f94fb0def32bef098e4c88
Thomas Stenhaug <thomas.stenhaug@gmail.com>
2019-12-17 18:57:48 +0100

    fixed insert-recipe

--- src/insert-recipe.py
+++ src/insert-recipe.py
@@ -5,8 +5,8 @@ import sys
 cnx = mysql.connector.connect(user='dm565sip', database='dm565', host='karvus.com', password='zipthat')
 cursor = cnx.cursor()
 
-recipe_sql_template = 'INSERT INTO recipe (id, title, instructions, total_energy, energy_per_serving, protein, fat, carbohydrates) VALUES ({id}, "{title}", "{instructions}", {nutrients[Totalkcal]}, {nutrients[kcal]}, {nutrients[protein]}, {nutrients[carbohydrates]};'
-ingredient_sql_template = 'INSERT INTO ingredient (recipe_id, amount, unit, name) VALUES ({id}, {amount}, "{unit}", "{name});'
+recipe_sql_template = 'INSERT INTO recipe (id, title, instructions, total_energy, energy_per_serving, protein, fat, carbohydrates) VALUES ({id}, "{title}", "{instructions}", {nutrients[Totalkcal]}, {nutrients[kcal]}, {nutrients[protein]}, {nutrients[fat]}, {nutrients[carbohydrates]});'
+ingredient_sql_template = 'INSERT INTO ingredients (recipe_id, amount, unit, name) VALUES ({id}, {amount}, "{unit}", "{name}");'
 
 def compute_statements(recipe_json):
     recipe_stmt = recipe_sql_template.format(**recipe_json)
@@ -21,5 +21,6 @@ if __name__ == '__main__':
         recipe_json = json.load(json_file)
     statements = compute_statements(recipe_json)
     for statement in statements:
+        # print(statement)
         cursor.execute(statement)
-        
+    cnx.commit()
